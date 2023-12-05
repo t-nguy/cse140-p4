f1ec845ff6d9a4896339c3331114137b6d478793
randytran9617 <146786673+randytran9617@users.noreply.github.com>
2023-12-04 17:22:11 -0800

    defense agent

--- pacai/student/defensetest.py
+++ pacai/student/defensetest.py
@@ -1,5 +1,8 @@
 from pacai.agents.capture.reflex import ReflexCaptureAgent
 from pacai.core.directions import Directions
+from pacai.agents.search.multiagent import MultiAgentSearchAgent
+import random
+
 
 class DefensiveReflexAgent(ReflexCaptureAgent):
     """
@@ -13,15 +16,47 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
         #print(self.index)
     
     def minimaxFunction(self, gameState, depth, agentIndex):
-        invaders = [a for a in enemies if a.isPacman() and a.getPosition() is not None]
-        if depth == 0 or gameState.isWin() or gameState.isLose():
-            return self.getEvaluationFunction()(gameState)
+        #print("test")
+        if depth == 0 and agentIndex in self.getTeam(gameState):
+            myState = gameState.getAgentState(agentIndex)
+            myPos = myState.getPosition()
+            #print("test")
+            invaders = []
+            for x in self.getOpponents(gameState):
+                enemyState = gameState.getAgentState(x)
+                enemyPos = enemyState.getPosition()
+                invaders.append(enemyPos)
+            #print("test")
+            invading = invaders[0]
+            for x in invaders:
+                if x[0] < invading[0]:
+                    invading = x
+            dists = [self.getMazeDistance(myPos, invading)]
+            #print(dists)
+            return dists
+
+        if depth == 0 and agentIndex in self.getOpponents(gameState):
+            #print("test")
+            return 1
+
+        if agentIndex >= gameState.getNumAgents():
+            agentIndex = 0
+            depth -= 1 
+            
 
         legalMoves = gameState.getLegalActions(agentIndex)
-        if len(legalMoves) == 0:
-            return self.getEvaluationFunction()(gameState)
+        #if len(legalMoves) == 0:
+            #return self.getEvaluationFunction()(gameState)
         
-        otherTeamSize = len(getOpponents(gameState))
+        otherTeamSize = len(self.getOpponents(gameState)) 
+        
+        if agentIndex in self.getTeam(gameState):
+            return min(self.minimaxFunction(gameState.generateSuccessor(agentIndex, actions),
+                                            depth, agentIndex + 1) for actions in legalMoves)
+        
+        if agentIndex in self.getOpponents(gameState):
+            return max(self.minimaxFunction(gameState.generateSuccessor(agentIndex, actions),
+                                            depth, agentIndex + 1) for actions in legalMoves)
 
         #for a in invaders:
 
@@ -32,6 +67,8 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
         myPos = myState.getPosition()
 
         #print(self.getOpponents(gameState))
+        #print(self.getTeam(gameState))
+        #print(gameState.getNumAgents())  
 
         # Computes whether we're on defense (1) or offense (0).
         features['onDefense'] = 1
@@ -64,7 +101,6 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
             if not gameState.isOnRedTeam(self.index):
                 x = 1
                 middleCoord = (width/2 + x,height/2)
-                #print(middleCoord)
                 test = (int(middleCoord[0]), int(middleCoord[1]))
 
                 while middleCoord in wallsCoord:
@@ -74,13 +110,29 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
                 middleDistance = [self.getMazeDistance(myPos, middleCoord)]
                 features['middle'] = min(middleDistance)
 
-
         if (len(invaders) > 0):
-            dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
-            features['invaderDistance'] = min(dists)
             
-        #if myState.isScaredGhost():
-            #print("Scared")
+            dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+            #print(dists)
+            features['invaderDistance'] = min(dists)
+            '''
+            legalMoves = gameState.getLegalActions()
+            scores = [self.minimaxFunction(gameState.generateSuccessor(0, actions), 1, 0)
+                    for actions in legalMoves]
+            bestScore = min(scores)
+            bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+            chosenIndex = random.choice(bestIndices)  # Pick randomly among the best.
+            features['invaderDistance'] = chosenIndex
+            #return legalMoves[chosenIndex]
+            '''
+
+        if myState.isScaredGhost():
+            run = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
+            for x in run:
+                print(run)
+                #if (x == 1) or (x == 2):       
+
+        
 
 
         if (action == Directions.STOP):
